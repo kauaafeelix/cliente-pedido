cd71a7ab9f8b6eaaa4aa574e1c83c7facc2d08af
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-20 21:46:52 -0300

    feat: primeiro commit

---
+++ cliente-pedido/src/main/java/com/weg/centroweg/cliente_pedido/ClientePedidoApplication.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.cliente_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePedidoApplication.class, args);
+	}
+
+}
---
+++ cliente-pedido/src/test/java/com/weg/centroweg/cliente_pedido/ClientePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.cliente_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
